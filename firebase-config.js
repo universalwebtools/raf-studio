868882a7870d85f83ac92d371b98fbd82b287eeb
Rafał Kukla <loliwtf.omg@gmail.com>
2026-08-31 09:59:56 +0200

    RAF.studio v3: dodaj konfiguracje Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3dwyRdq6dcgl-_yhK-9QEgufA3RLpPG0",
+  authDomain: "rafstudiogaleria.firebaseapp.com",
+  databaseURL: "https://rafstudiogaleria-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rafstudiogaleria",
+  storageBucket: "rafstudiogaleria.firebasestorage.app",
+  messagingSenderId: "53550728942",
+  appId: "1:53550728942:web:dbcf91a86807ada97d15ff",
+  measurementId: "G-GJ9LZR48BT"
+};
+export const ADMIN_UID = "NLYAfU5UNcfldH4k2x2r9m1g4Vx1";
+export const WEBSITE_ROOT = "website";
