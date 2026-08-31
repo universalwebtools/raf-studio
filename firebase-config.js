b53f42888def455f93b58fa0305dd4fc58dac150
Rafał Kukla <loliwtf.omg@gmail.com>
2026-08-31 20:26:51 +0200

    Podłącz Realtime Database do RAF.studio

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyC3sl83q1pmYx5bgEyCPu659xvc5Nsajo0",
   authDomain: "raf-studio-strona.firebaseapp.com",
+  databaseURL: "https://raf-studio-strona-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "raf-studio-strona",
   storageBucket: "raf-studio-strona.firebasestorage.app",
   messagingSenderId: "302300072088",
