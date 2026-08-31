3b1554d63013cbadb3311041df268058c296f1ab
Rafał Kukla <loliwtf.omg@gmail.com>
2026-08-31 20:21:19 +0200

    Podłącz RAF.studio do nowego projektu Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyB3dwyRdq6dcgl-_yhK-9QEgufA3RLpPG0",
-  authDomain: "rafstudiogaleria.firebaseapp.com",
-  databaseURL: "https://rafstudiogaleria-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "rafstudiogaleria",
-  storageBucket: "rafstudiogaleria.firebasestorage.app",
-  messagingSenderId: "53550728942",
-  appId: "1:53550728942:web:dbcf91a86807ada97d15ff",
-  measurementId: "G-GJ9LZR48BT"
+  apiKey: "AIzaSyC3sl83q1pmYx5bgEyCPu659xvc5Nsajo0",
+  authDomain: "raf-studio-strona.firebaseapp.com",
+  projectId: "raf-studio-strona",
+  storageBucket: "raf-studio-strona.firebasestorage.app",
+  messagingSenderId: "302300072088",
+  appId: "1:302300072088:web:4417bb7718faf7d20e68ed",
+  measurementId: "G-EL9YB2P2PS"
 };
-export const ADMIN_UID = "NLYAfU5UNcfldH4k2x2r9m1g4Vx1";
+export const ADMIN_UID = "";
 export const WEBSITE_ROOT = "website";
