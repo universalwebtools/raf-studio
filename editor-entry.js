65dfb3e317c388698ea6fbe46ed4c2d89be1d1ac
Rafał Kukla <loliwtf.omg@gmail.com>
2026-09-01 18:47:10 +0200

    Bump stable drag cache for editor v4

--- editor-entry.js
+++ editor-entry.js
@@ -14,7 +14,7 @@ if(mode==='direct'||mode==='1'){
       }else{
         await import('./editor-prep-v34.js?v=3.4.0');
         await import('./direct-editor-v3.js?v=3.0.1');
-        await import('./direct-drag-v34.js?v=3.4.0');
+        await import('./direct-drag-v34.js?v=3.5.0');
         await import('./direct-publish-v32.js?v=3.4.0');
         await import('./editor-ui-v4.js?v=4.0.0');
       }
